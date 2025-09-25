8d6288252d299467ef300bf078dea093539c9f16
Adarsh <173589989+Adarsh-141@users.noreply.github.com>
2025-09-26 00:19:54 +0530

    Set DEBUG to False for production readiness

--- aakar/settings.py
+++ aakar/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['aakaariitbombay.org','43.204.211.168','www.aakaariitbombay.org.in', '127.0.0.1','www.aakaariitbombay.org']
 
